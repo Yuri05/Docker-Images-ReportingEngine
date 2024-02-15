9cdc16128957567f91ede3d8aa71e69f77012774
Yuri05 <Yuri05@github.com>
2024-02-15 16:24:12 +0100

    test4A

--- .github/workflows/install_packages_bionic.sh
+++ .github/workflows/install_packages_bionic.sh
@@ -5,3 +5,4 @@ wget -nv --no-check-certificate https://ci.appveyor.com/api/projects/open-system
 
 # Install packages
 R CMD INSTALL /tmp_setup/ospsuite_ubuntu18.tar.gz --install-tests
+R --no-save -e "remotes::install_github('Open-Systems-Pharmacology/OSPSuite.ReportingEngine', ref ='develop')"
