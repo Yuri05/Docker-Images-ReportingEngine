6e6cdbbad2803fd5cf28be90a926e16cfeb662c3
Yuri05 <Yuri05@github.com>
2024-02-15 17:03:40 +0100

    test4A

--- .github/workflows/install_packages_cran.sh
+++ .github/workflows/install_packages_cran.sh
@@ -2,5 +2,5 @@
 
 # Install packages
 R --no-save -e "install.packages(c('remotes', 'dplyr', 'tidyr', 'purrr', 'patchwork', 'readxl','data.table', 'gridtext', 'ggtext'), repos='http://cran.rstudio.com/')"
-R --no-save -e "remotes::install_github('Open-Systems-Pharmacology/OSPSuite.RUtils', ref ='develop', INSTALL_opts = "--install-tests")"
-R --no-save -e "remotes::install_github('Open-Systems-Pharmacology/TLF-Library', ref ='develop', INSTALL_opts = "--install-tests")"
+R --no-save -e "remotes::install_github('Open-Systems-Pharmacology/OSPSuite.RUtils', ref ='develop', INSTALL_opts = '--install-tests')"
+R --no-save -e "remotes::install_github('Open-Systems-Pharmacology/TLF-Library', ref ='develop', INSTALL_opts = '--install-tests')"
